7a4c57405c7015ffdf4b8abdabbcb6b68343ae4c
Kafayat6 <101401730+Kafayat6@users.noreply.github.com>
2022-07-05 21:38:01 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,12 @@
-g
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if (my_list == []):
+        return None
+    for integer in my_list:
+        new_list.append(integer)
+    if (idx < 0 or idx > len(my_list) - 1):
+        return my_list
+    else:
+        new_list[idx] = element
+        return new_list
