2f2f6fd1be2b725eeb5625cda52236169845f4dc
Kafayat6 <101401730+Kafayat6@users.noreply.github.com>
2022-07-05 21:39:29 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,8 @@
-g
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for listInt in matrix:
+        for integer in listInt:
+            print('{:d}'.format(integer), end='')
+            if integer != listInt[-1]:
+                print(' ', end='')
+        print("")
